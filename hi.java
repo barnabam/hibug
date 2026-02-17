c0ebe96528024b8970a05944c026bccbc7f765c9
barnabam <mathebarni29@gmail.com>
2026-02-17 17:49:57 +0100

    java

---
+++ hi.java
@@ -0,0 +1,9 @@
+// java hi.java 
+
+class HiBug {
+  public static void main(String[] args) {
+    System.out.println("JVM version - " + System.getProperty("java.version"));
+    System.out.println("hi bug!"); 
+  }
+}
+
