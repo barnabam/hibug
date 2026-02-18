a76691137415c355e75bf0043d8f2633aef27098
barnabam <mathebarni29@gmail.com>
2026-02-18 13:55:56 +0100

    matlab

---
+++ hi.m
@@ -0,0 +1,8 @@
+% open Matlab and run "run hi.m" in the Command Window
+% or
+% matlab -nodesktop -nosplash -r "run("hi.m"); exit;" 
+
+version_message = sprintf("Matlab version: %s", version);
+disp(version_message)
+disp("hi bug!")
+
